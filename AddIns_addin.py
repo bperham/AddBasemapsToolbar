bbee6c0530adb47383dd6842651c2e2ec5c6f860
Bob Perham <34038083+bperham@users.noreply.github.com>
2017-12-18 14:02:37 -0800

    Add files via upload

---
+++ AddIns_addin.py
@@ -0,0 +1,48 @@
+import arcpy
+import pythonaddins
+
+class AddBingMaps(object):
+    """Implementation for AddIns_addin.button_AddBingMaps (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        # get the map document
+        mxd = arcpy.mapping.MapDocument("CURRENT")
+
+        # get the data frame
+        df = arcpy.mapping.ListDataFrames(mxd,"*")[0]
+
+        # specify the layer
+        newlayer = arcpy.mapping.Layer(r"N:\GIS\Layer Files\Bing Maps.lyr")
+
+        # add the layer to the map at the bottom of the TOC in data frame 0
+        arcpy.mapping.AddLayer(df, newlayer,"BOTTOM")
+
+        # Refresh things
+        arcpy.RefreshActiveView()
+        arcpy.RefreshTOC()
+        pass
+        
+class AddTopoBasemap(object):
+    """Implementation for AddIns_addin.button_AddTopoBasemap (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        # get the map document
+        mxd = arcpy.mapping.MapDocument("CURRENT")
+
+        # get the data frame
+        df = arcpy.mapping.ListDataFrames(mxd,"*")[0]
+
+        # specify the layer
+        newlayer = arcpy.mapping.Layer(r"N:\GIS\Layer Files\Topo Maps.lyr")
+
+        # add the layer to the map at the bottom of the TOC in data frame 0
+        arcpy.mapping.AddLayer(df, newlayer,"BOTTOM")
+
+        # Refresh things
+        arcpy.RefreshActiveView()
+        arcpy.RefreshTOC()
+        pass
