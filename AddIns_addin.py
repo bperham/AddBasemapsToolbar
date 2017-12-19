7f67d7aeba3183f170d499532f563398dedaeca3
Bob Perham <34038083+bperham@users.noreply.github.com>
2017-12-19 15:01:44 -0800

    Update AddIns_addin.py
    Added Parcel Coverage class

--- AddIns_addin.py
+++ AddIns_addin.py
@@ -47,3 +47,25 @@ class AddTopoBasemap(object):
         arcpy.RefreshActiveView()
         arcpy.RefreshTOC()
         pass
+    class AddParcelCoverage(object):
+    """Implementation for AddIns_addin.button_AddParcelCoverage (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        # get the map document
+        mxd = arcpy.mapping.MapDocument("CURRENT")
+
+        # get the data frame
+        df = arcpy.mapping.ListDataFrames(mxd,"*")[0]
+
+        # specify the layer
+        newlayer = arcpy.mapping.Layer(r"N:\GIS\Layer Files\PPLTD Parcel Coverage.lyr")
+
+        # add the layer to the map at the bottom of the TOC in data frame 0
+        arcpy.mapping.AddLayer(df, newlayer,"BOTTOM")
+
+        # Refresh things
+        arcpy.RefreshActiveView()
+        arcpy.RefreshTOC()
+        pass
