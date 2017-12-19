42f88286b85f41fe9016015ad4494449daed9396
Bob Perham <34038083+bperham@users.noreply.github.com>
2017-12-19 15:42:25 -0800

    Update AddIns_addin.py
    Previous update didn't include world imagery.

--- AddIns_addin.py
+++ AddIns_addin.py
@@ -47,8 +47,9 @@ class AddTopoBasemap(object):
         arcpy.RefreshActiveView()
         arcpy.RefreshTOC()
         pass
-    class AddParcelCoverage(object):
-    """Implementation for AddIns_addin.button_AddParcelCoverage (Button)"""
+
+    class AddWorldImagery(object):
+    """Implementation for AddIns_addin.button_AddWorldImagery (Button)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
@@ -60,7 +61,30 @@ class AddTopoBasemap(object):
         df = arcpy.mapping.ListDataFrames(mxd,"*")[0]
 
         # specify the layer
-        newlayer = arcpy.mapping.Layer(r"N:\GIS\Layer Files\PPLTD Parcel Coverage.lyr")
+        newlayer = arcpy.mapping.Layer(r"N:\GIS\Layer Files\World Imagery.lyr")
+
+        # add the layer to the map at the bottom of the TOC in data frame 0
+        arcpy.mapping.AddLayer(df, newlayer,"BOTTOM")
+
+        # Refresh things
+        arcpy.RefreshActiveView()
+        arcpy.RefreshTOC()
+        pass
+
+    class AddParcelData(object):
+    """Implementation for AddIns_addin.button_AddParcelData (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        # get the map document
+        mxd = arcpy.mapping.MapDocument("CURRENT")
+
+        # get the data frame
+        df = arcpy.mapping.ListDataFrames(mxd,"*")[0]
+
+        # specify the layer
+        newlayer = arcpy.mapping.Layer(r"N:\GIS\Layer Files\PPLTD Parcel Data.lyr")
 
         # add the layer to the map at the bottom of the TOC in data frame 0
         arcpy.mapping.AddLayer(df, newlayer,"BOTTOM")
